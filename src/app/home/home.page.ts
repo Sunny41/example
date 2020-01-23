45599b3d857aa67d543e5cb05c64549f2f4c7a8f
Sonja <sonja.czernotzky@loesdau.de>
2020-01-23 08:53:25 +0100

    Bur() testing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public eMail;
+  public password;
+  public message;
+  public hidden;
+  public eye = 'eye';
+  public passwordType = 'password';
+  public passwordShown = false;
+  @ViewChild('mail' , { read: ElementRef, static: false }) mail: ElementRef;
+  @ViewChild('pw' , { read: ElementRef, static: false }) pw: ElementRef;
 
-  constructor() {}
 
-  doSomething(){
+  constructor(private navCtrl: NavController, private keyboard: Keyboard) {}
+
+  doSomething() {
     console.log('triggered');
   }
 
+  forgotPassword() {}
+
+  login() {
+
+    this.keyboard.hide();
+    this.pw.nativeElement.setBlur();
+    this.mail.nativeElement.blur();
+
+  }
+
+  cancel() {
+    this.navCtrl.navigateRoot('/home');
+  }
+
+  skip() {}
+
+  showPassword() {
+    if (this.passwordShown) {
+      this.passwordShown = false;
+      this.passwordType = 'password';
+      this.eye = 'eye-off';
+    } else {
+      this.passwordShown = true;
+      this.passwordType = 'text';
+      this.eye = 'eye';
+    }
+  }
 }
